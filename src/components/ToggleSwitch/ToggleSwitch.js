0852201a99e80b0809e1685e01d05ec77916cdac
pstaal <peter.staal@gmail.com>
2022-10-14 08:53:38 +0200

    debugging

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -1,14 +1,15 @@
 import "./ToggleSwitch.css";
+import { useState } from 'react'
 
 function ToggleSwitch() {
-
+    const [checked, setChecked] = useState(false)
     return (
         <div className="container">
             <label className="switch" htmlFor="checkbox">
-                <input type="checkbox" id="checkbox" />
+                <input type="checkbox" id="checkbox" onChange={((e) => setChecked(e.target.checked))} />
                 <div className="slider round">
-                    <p className="temp">F</p>
-                    <p className="temp">C</p>
+                    <p className={`temp ${!checked && 'temp_active'}`}>F</p>
+                    <p className={`temp ${checked && 'temp_active'}`}>C</p>
                 </div>
             </label>
         </div>
